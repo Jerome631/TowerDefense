db04c469bf9eb2e5f35ccc10afed27b0fd930079
Jerome631 <jeromedevassy01@gmail.com>
2021-11-14 20:03:24 +0000

    Tile Set System
    Made a system used to set tiles for the levels.

--- Solo Game/Assets/Scripts/LevelManager.cs
+++ Solo Game/Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,21 +24,35 @@ public class LevelManager : MonoBehaviour
 
     private void createlevel()
     {
+        string[] mapData = ReadLevelText();
+
+        int mapX = mapData[0].ToCharArray().Length;
+        int mapY = mapData.Length;
+
         Vector3 worldstart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));   
         
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < mapY; i++)
         {
-            for (int j = 0; j < 10; j++)
+            char[] newtiles = mapData[i].ToCharArray();
+            for (int j = 0; j < mapX; j++)
             {
-                PlaceTile(i,j, worldstart);
+                PlaceTile(newtiles[j].ToString(), j, i, worldstart) ;
             }
         }
     }
     
-    private void PlaceTile(int i, int j, Vector3 worldstart)
+    private void PlaceTile(string tiletype, int i, int j, Vector3 worldstart)
     {
-        GameObject newtile = Instantiate(tileprefabs[0]);
+        int tileindex = int.Parse(tiletype);
+        GameObject newtile = Instantiate(tileprefabs[tileindex]);
         newtile.transform.position = new Vector3(worldstart.x+TileSize * i, worldstart.y-TileSize * j, 0);
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset binddata = Resources.Load("Level") as TextAsset;
+        string data = binddata.text.Replace(Environment.NewLine, string.Empty);
+        return data.Split('-');
     }
 
 }
