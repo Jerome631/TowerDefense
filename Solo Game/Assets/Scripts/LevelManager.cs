8035bfcb729f23c3c896b26056cbef152449e3c3
Jerome631 <jeromedevassy01@gmail.com>
2021-11-20 22:51:03 +0000

    Camera Mechanics
    Established camera mechanics

--- Solo Game/Assets/Scripts/LevelManager.cs
+++ Solo Game/Assets/Scripts/LevelManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public GameObject[] tileprefabs;
+    public CameraMovement cameraMovement; 
 
     public float TileSize
     {
@@ -26,9 +27,13 @@ public class LevelManager : MonoBehaviour
     {
         string[] mapData = ReadLevelText();
 
+        //Calculate x and y of map size
         int mapX = mapData[0].ToCharArray().Length;
         int mapY = mapData.Length;
 
+        Vector3 maxTile = Vector3.zero;
+
+        //Calculates the world start point(Top left of screen)
         Vector3 worldstart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));   
         
         for (int i = 0; i < mapY; i++)
@@ -36,16 +41,25 @@ public class LevelManager : MonoBehaviour
             char[] newtiles = mapData[i].ToCharArray();
             for (int j = 0; j < mapX; j++)
             {
-                PlaceTile(newtiles[j].ToString(), j, i, worldstart) ;
+                //Palces tile
+                maxTile=PlaceTile(newtiles[j].ToString(), j, i, worldstart) ;
             }
         }
+
+        cameraMovement.SetLimit(new Vector3(maxTile.x+TileSize, maxTile.y-TileSize));
     }
     
-    private void PlaceTile(string tiletype, int i, int j, Vector3 worldstart)
+    private Vector3 PlaceTile(string tiletype, int i, int j, Vector3 worldstart)
     {
+        //Changes tiletype to int
         int tileindex = int.Parse(tiletype);
+
+        //Creates a new tile and makes a reference to that tile in newtile
         GameObject newtile = Instantiate(tileprefabs[tileindex]);
+
+        //Uses the new tile variable to change the position
         newtile.transform.position = new Vector3(worldstart.x+TileSize * i, worldstart.y-TileSize * j, 0);
+        return newtile.transform.position;
     }
 
     private string[] ReadLevelText()
